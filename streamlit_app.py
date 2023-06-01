aba64dcea55f0fff5f491e6a8c4e746bdd8f20a3
atulsaini0605 <135156773+atulsaini0605@users.noreply.github.com>
2023-06-01 13:21:09 -0500

    Update streamlit_app.py
    Deleted the Jason on screen output

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-streamlit.text(fruityvice_response.json()) #write the data to screen
+#streamlit.text(fruityvice_response.json()) #write the data to screen
 
 # normalize the data 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
